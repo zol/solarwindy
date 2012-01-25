d8595be5dadc31d2b6a6b1357dd13aa02dcaffe6
Zoltan Olah <zol@me.com>
2012-01-24 16:51:11 -0800

    Added AggregateObservation logging.

--- solarwindy/Logger.cpp
+++ solarwindy/Logger.cpp
@@ -47,6 +47,43 @@ void Logger::PrintReading(float temp, float wind_speed,
   dtostrf(temp, 3, 2, temp_buffer);
   dtostrf(wind_speed, 3, 2, wind_speed_buffer);
   
-  Printf("-- Temp %sc, Wind %skn %s at %u seconds since boot.\n",
+  Printf("-- Temp %sc, Wind %skn %s at %u seconds since boot.\r\n",
     temp_buffer, wind_speed_buffer, direction, (time / 1000));
+}
+
+void Logger::PrintAggregateReading(float average_wind_speed,
+    float wind_gust,
+    unsigned long wind_gust_time,
+    const char *wind_gust_direction,
+    const char *most_frequent_wind_direction,
+    unsigned long elapsed_time,
+    unsigned long time) {
+    
+  // char wind_gust_buffer[16];
+  // dtostrf(wind_gust, 3, 2, wind_gust_buffer);
+  //   
+  // Serial.println(most_frequent_wind_direction);
+  // Printf("== Gust %skn %s at %u seconds since boot. Avg direction \
+  //   %s, readings over %u secs, time since boot is %u.\r\n",
+  //   wind_gust_buffer,
+  //   wind_gust_direction,    
+  //   (wind_gust_time / 1000),
+  //   most_frequent_wind_direction,
+  //   (elapsed_time / 1000),
+  //   (time / 1000));
+  
+  Serial.print("== Avg Speed ");
+  Serial.print(average_wind_speed);
+  Serial.print("kn Gust ");
+  Serial.print(wind_gust);
+  Serial.print(" at ");
+  Serial.print(wind_gust_time);
+  Serial.print(" seconds since boot. Gust direction ");
+  Serial.print(wind_gust_direction);
+  Serial.print(" Avg Wind Direction ");
+  Serial.print(most_frequent_wind_direction);
+  Serial.print(" Readings over elapsed time:");
+  Serial.print(elapsed_time / 1000);
+  Serial.print(" secs, time since boot is ");
+  Serial.println(time / 1000);
 }
--- solarwindy/Logger.h
+++ solarwindy/Logger.h
@@ -4,17 +4,22 @@
 #ifndef SOLARWINDY_LOGGER_H
 #define SOLARWINDY_LOGGER_H
 
-#include "ObservationStore.h"
-
 class Logger {
  public:
    static void Init();
    static void Fatal(const char *message);
    static void Debug(const char *message);
    static void Printf(const char *format, ...);
-   static void PrintReading(float temp, float wind_speed, 
+   static void PrintReading(float temp, float wind_speed,
      const char *direction, unsigned long time);
-   
+   static void PrintAggregateReading(float average_wind_speed,
+     float wind_gust,
+     unsigned long wind_gust_time,
+     const char *wind_gust_direction,
+     const char *most_frequent_wind_direction,
+     unsigned long elapsed_time,
+     unsigned long time);
+
   private:
    static const int kBufferSize = 128; // Largest string we will print
 };
