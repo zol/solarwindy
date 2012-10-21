00e03f32e52b6f9740db9b2117867941796eeb0e
Zoltan Olah <zol@me.com>
2012-10-21 14:03:46 -0700

    Simplified to upload using the ethernet shield.

--- README.markdown
+++ README.markdown
@@ -1,21 +1,14 @@
-# Description
+# Solarwindy
 
-The solarwindy is an arduino based Autonomous Weather Station. It uploads weather data to a webserver using a GSM quad band chip and is solar powered for totally autonomous remote deployment. It is ideal for sports such as paragliding and kiteboarding that benefit from localized near real time weather info. Here you will find step by step instructions on how to make it, where to buy the components and the software to run it.
+Solarwindy is an arduino based weather station. He reads wind and temperature and shoots it up to Sky Bob (he's in the cloud. or he's a cloud. whatever.) Solarwindy lives near Lake Meritt, Oakland in the great state of California. Someday Solarwindy will grow a gprs shield and attached to a solar panel will roam free.
 
-# Project Goals
+# Sky Bob
 
-* Price: As low as possible.
-* Repeatability: No exotic components, everything must be readily available online and internationally shipped.
-* Complete DIY instructions: Complete step-by-step instructions and parts list so that someone with minimal technical experience and tools can build the solarwindy.
-* Robustness: Given the price constraint, maintain a level of hardware and software robustness that will ensure the solarwindy survives outdoors and is easy to debug given any issues.
-* Community: The project is totally open source, anyone can contribute improvements, fixes and documentation.
+[Our friend in the sky.](http://skybob.meteor.com/ "Sky Bob lives here.") Sky Bob is omnipotent, mostly benevolent but sometimes downright nasty. In any case, he'll tell you exactly what's up. His best friend is [meteor](http://meteor.com/ "Meteor JS").
 
-# Instructions and Parts List
-
-Coming soon, in the meantime, this is Rodney:
-
-<div style="float: right"><img class="background-color: red;" alt="Rodney" src="https://github.com/zol/solarwindy/raw/master/resources/rodney.gif"></div>
+<img alt="Sky Bob" src="https://github.com/zol/solarwindy/raw/master/resources/skybob.png" />
 
 # News
 
+20th Oct 2012 - Deployed Sky Bob and modified solarwindy to use the Ethernet Shield to transmit to skybob.
 4th Jan 2012 - Project is started. I have the arduino, anemometer, wind vane and temperature sensor and am commencing work on writing an initial prototype of the software.
--- solarwindy/solarwindy.ino
+++ solarwindy/solarwindy.ino
@@ -1,35 +1,42 @@
-// Arduino Sketch for solarwindy, a solar powered quad band autonomous
+// Arduino Sketch for solarwindy, a an autonomous
 // weather station (http://www.github.com/zol/solarwindy).
 //
-// 1. We read the sensors every kReadInterval.
-// 2. We keep a rolling store of kTransmitInterval's worth of data.
-// 3. At every kTransmitInterval, we upload a summary of our stored data.
+// At every kTransmitInterval, we read the sensors and upload them via http GET.
 //
 // Zoltan Olah (zol@me.com) released under the MIT license on Jan 13 2012.
 
+#include <Ethernet.h>
+#include <SPI.h>
 #include "ArgentAnemometer.h"
 #include "ArgentVane.h"
 #include "TMP36TempSensor.h"
-#include "ObservationStore.h"
 #include "Logger.h"
 #include "MemoryFree.h"
+#include "ObservationStore.h"
 
-const int kTransmitInterval = 1; // Min intervals we use between transmits
-const int kReadInterval = 5; // Sec intervals we read and log the sensors
-const int kDebugLedPin = 13; // We flash this led to signal program flow
+const char *kHost = "10.0.1.6"; // Host running skybob.
+const int kPort = 3000; // Skybob's port.
 
-unsigned long lastReadTime = 0; // Used to calculate the next time to read
-unsigned long lastTransmitTime = 0; // As above
+const int kTransmitInterval = 5; // Sec intervals we use between transmits
+const int kDebugLedPin = 5; // We flash this led to signal program flow
+
+const uint8_t kMac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+unsigned long lastTransmitTime = 0; // Stores the last time we transmitted
 
 unsigned int debug_led_state = LOW;
 
 ArgentAnemometer anemometer(2);
 ArgentVane vane(A0);
 TMP36TempSensor temp(A1);
-ObservationStore store(12); // 1 minute worth of observations
+EthernetClient client;
 
 void setup() {
   Logger::Init();
+  
+  Logger::Printf("Obtaining dhcp lease...\n");
+  Ethernet.begin(mac);
 
   vane.Init();
   temp.Init();
@@ -37,20 +44,37 @@ void setup() {
 
   pinMode(kDebugLedPin, OUTPUT);
 
-  if (store.Init() == false)
-    Logger::Fatal("ObservationStore Init() failed. Check free memory.");
-    
-  Logger::Printf("Solarwindy initalized. Free Memory: %d bytes.\n", freeMemory());
+  Logger::Printf("Solarwindy initalized. Free Memory: %d bytes. IP Address:", freeMemory());
+  Serial.println(Ethernet.localIP());
+}
+
+void transmit(EthernetClient *client, float temp, float wind_speed, const char *direction) {
+  char body[256];
+  char temp_buffer[16];
+  char wind_speed_buffer[16];
+  
+  dtostrf(temp, 3, 2, temp_buffer);
+  dtostrf(wind_speed, 3, 2, wind_speed_buffer);
+  
+  snprintf(body, 256, "GET /create_observation?windSpeed=%s&windDirection=%s&temp=%s HTTP/1.1\r\n\r\n",
+    wind_speed_buffer, direction, temp_buffer);
+  
+  Logger::Printf("Transmitting...");
+  if (client->connect(kHost, kPort)) {
+    client->print(body);  
+    Logger::Printf("done\n");
+  } else {
+    Logger::Debug("Failed to connect to host.");
+  }
 }
 
 void loop() {
   unsigned long time = millis(); //  Current time since boot up
-  unsigned long readInterval = time - lastReadTime;
   unsigned long transmitInterval = time - lastTransmitTime;
 
   //  This block will run every kReadInterval
-  if (readInterval >= (kReadInterval * 1000)) {
-    lastReadTime = time;
+  if (transmitInterval >= (kTransmitInterval * 1000)) {
+    lastTransmitTime = time;
     
     // flip the led each time we go aroudn the loop
     debug_led_state = debug_led_state == HIGH ? LOW : HIGH;
@@ -58,35 +82,16 @@ void loop() {
 
     vane.Read();
     temp.Read();
-    anemometer.Read(readInterval);
+    anemometer.Read(transmitInterval);
     anemometer.ResetTicks();
 
-    ObservationStore::Observation observation;
-    observation.wind_speed = anemometer.ComputeKnots();
-    observation.wind_segment = vane.ComputeSegment();
-    observation.time = time;
-
-    store.Add(observation);
-    
-    Logger::PrintReading(temp.ComputeCelsius(), observation.wind_speed,
+    Logger::PrintReading(temp.ComputeCelsius(), anemometer.ComputeKnots(),
       vane.ComputeDirection(), time);
+    transmit(&client, temp.ComputeCelsius(), anemometer.ComputeKnots(),
+      vane.ComputeDirection());
   }
   
-  //  This block will run every kTransmitInterval
-  if (transmitInterval >= (static_cast<unsigned long>(kTransmitInterval) * 1000 * 60)) {
-    lastTransmitTime = time;
-
-    ObservationStore::AggregateObservation ao = 
-      store.ComputeAggregate();
-
-    Logger::PrintAggregateReading(ao.average_wind_speed, 
-      ao.wind_gust, 
-      ao.wind_gust_time, 
-      ArgentVane::SegmentToDirection(ao.wind_gust_segment),
-      ArgentVane::SegmentToDirection(ao.most_frequent_wind_segment), 
-      ao.elapsed_time,
-      time);
-    
-    // TODO: upload the aggregate values
-  }
+  // chomp up bytes and ensure we disconnect to allow future requests to work.
+  if (client.available()) { char c = client.read(); }
+  if (!client.connected()) { client.stop(); }
 }
